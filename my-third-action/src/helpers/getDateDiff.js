a822c065967a14ee597cd125df3de803f3c33115
Edward Sweezey <edwardsweezey@Edwards-MacBook-Pro.local>
2023-01-31 16:02:21 -0500

    linting continued 3

--- my-third-action/src/helpers/getDateDiff.js
+++ my-third-action/src/helpers/getDateDiff.js
@@ -4,10 +4,9 @@ export function getDateDiff(date) {
   const diff = now - date
   const diffInDays = diff / (1000 * 60 * 60 * 24)
   if (diffInDays) {
-
   return diffInDays
-
   }
+
   console.log('this is a test of line block spacing')
 }
 
@@ -16,22 +15,15 @@ export function removeMultiLine(x) {
   if(x === 1) {
     console.log('this will always be true')
   }
+
   array.forEach((item) => {
     console.log(item)
   })
   console.log('done')
-
-  
 }
-
-
 
 export function removeMultiLine2(y) {
   if(y === 1) {
     console.log('this will always be true')
   }
-
-
-
-  
 }
