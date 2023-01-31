80194c27132ff558fc8c46100f474371b33da77c
Edward Sweezey <edwardsweezey@Edwards-MacBook-Pro.local>
2023-01-31 16:42:11 -0500

    testing new linting configuration

--- my-third-action/src/helpers/getDateDiff.js
+++ my-third-action/src/helpers/getDateDiff.js
@@ -1,18 +1,23 @@
-
 export function getDateDiff(date) {
   const now = new Date()
   const diff = now - date
   const diffInDays = diff / (1000 * 60 * 60 * 24)
+
   if (diffInDays) {
-  return diffInDays
+    return diffInDays
   }
+
+
 
   console.log('this is a test of line block spacing')
 }
 
+
+
+
 export function removeMultiLine(x) {
   const array = [1, 2, 3]
-  if(x === 1) {
+  if (x === 1) {
     console.log('this will always be true')
   }
 
@@ -22,8 +27,11 @@ export function removeMultiLine(x) {
   console.log('done')
 }
 
+
+
 export function removeMultiLine2(y) {
-  if(y === 1) {
+  if (y === 1) {
+    
     console.log('this will always be true')
   }
 }
